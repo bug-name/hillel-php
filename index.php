7e482e650f5bfc000f966ea228222a88f3831864
anohinden1991@gmail.com <Ad5336953369>
2021-07-16 22:13:33 +0300

    lesson 2

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
-<?php echo 'php';
+<?php /*
+$separator = ' ';
+var_export(explode($separator,"good,morning, fallows,ttt"));
+ ---
+ $our_string = '1111111';
+echo '$our_string';
+ --
+ echo 2 + '20 слона'
+ ---
+ $color = 'white ';
+$plant = 'flower';
+echo $color . $plant;
+ */ ?>
+
+
+
+<?php
+$num1 = 2;
+$num2 = 3;
+var_export($num1 === $num2);
+
+
+
 
