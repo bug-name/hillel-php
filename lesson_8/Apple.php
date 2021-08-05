7e998810554ffd5bb9c274bd85f865759849b37e
anohinden1991@gmail.com <Ad5336953369>
2021-08-05 22:35:47 +0300

    lesson_8

---
+++ lesson_8/Apple.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace lesson_8;
+
+class Apple
+{
+    public function __construct($color)
+    {
+        $this->color = $color;
+    }
+
+    public function __destruct()
+    {
+
+    }
+}
+
+
+
---
+++ lesson_8/SecondClass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lesson_8;
+
+class SecondClass
+{
+    public function __construct($color)
+    {
+    $this->color = $color;
+    }
+
+    public function __destruct()
+    {
+
+    }
+}
