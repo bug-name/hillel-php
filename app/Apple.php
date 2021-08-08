03de30d2aacc8821239bf11743ddbaeff890a8bd
anohinden1991@gmail.com <Ad5336953369>
2021-08-08 20:05:52 +0300

    lesson_9

--- app/Apple.php
+++ app/Apple.php
@@ -4,14 +4,8 @@ namespace App;
 
 class Apple
 {
-    public function __construct($color)
-    {
-        $this->color = $color;
-    }
-
-    public function __destruct()
-    {
-
+    public function index() {
+        print_r('Apple');
     }
 }
 
