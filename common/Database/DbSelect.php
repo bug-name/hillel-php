1cd24b8633e84a1d040dbaa2555b3d64519397d3
anohinden1991@gmail.com <Ad5336953369>
2021-09-01 21:14:14 +0300

    lesson_15

--- common/Database/DbSelect.php
+++ common/Database/DbSelect.php
@@ -1,24 +1,106 @@
 <?php
 namespace Common\Database;
 
-class DbSelect
+use Exception;
+
+class DbSelect extends DbWhere
 {
-    public function SelectString($dbName, $selectItem = '*', $joinType = '', $joinDBName = '', $onColumn1 = '', $onColumn2 = '')
+    protected $tableName;
+    protected $columnsName = '*';
+    protected $orderBy;
+    protected $join;
+    protected $limit;
+    protected $where;
+
+    /**
+     * @param mixed $tableName
+     */
+    public function setTableName($tableName)
     {
-        if ($joinType !== '' && $joinDBName != '') {
-            $joinPart =  ' '. $joinType .' '. $joinDBName .' ';
-        } else {
-            $joinPart = '';
-        }
+        $this->tableName = $tableName;
+    }
 
-        if ($onColumn1 !== '' && $onColumn2 != '') {
-            $onPart = ' on ' . $onColumn1 . ' = ' . $onColumn2;
-        } else {
-            $onPart = '';
-        }
+    /**
+     * @param mixed $columnsName
+     */
+    public function setColumnsName($columnsName)
+    {
+        $this->columnsName = $columnsName;
+    }
 
-        return  'SELECT ' . $selectItem .
-                ' FROM ' . $dbName .
-                $joinPart . $onPart;
+    /**
+     * @param mixed $orderBy
+     */
+    public function setOrderBy($orderBy)
+    {
+        $this->orderBy = $orderBy;
+    }
+
+    /**
+     * @param mixed $join
+     */
+    public function setJoin($join)
+    {
+        $this->join = $join;
+    }
+
+    /**
+     * @param mixed $limit
+     */
+    public function setLimit($limit)
+    {
+        $this->limit = $limit;
+    }
+
+    public function selectString()
+    {
+        $selectString = 'SELECT ' . $this->buildColumns()
+                        . ' FROM ' . $this->buildTableName();
+    }
+
+    protected function buildColumns()
+    {
+        if (is_array($this->columnsName)) {
+            $columnString = '';
+            foreach ($this->columnsName as $key => $value) {
+                if(!empty($columnString)){
+                    $columnString .= ',';
+                }
+                if (is_int($key)) {
+                    $columnString .= $value;
+                } else {
+                    $columnString .= $value . ' as ' . $key;
+                }
+            }
+            return $columnString;
+
+        } else if (is_string($this->columnsName)) {
+            return $this->columnsName;
+        } else {
+            throw new Exception('parsing columns not found');
+        }
+    }
+
+    protected function buildTableName()
+    {
+        if (is_array($this->tableName)) {
+            $tableString = '';
+            foreach ($this->tableName as $key => $value) {
+                if(!empty($tableString)){
+                    $tableString .= ',';
+                }
+                if (is_int($key)) {
+                    $tableString .= $value;
+                } else {
+                    $tableString .= $value . ' as ' . $key;
+                }
+            }
+            return $tableString;
+
+        } else if (is_string($this->tableName)) {
+            return $this->tableName;
+        } else {
+            throw new Exception('parsing columns not found');
+        }
     }
 }
